2e25f208b7b161049fefc787680a1e77aeb276e7
Salman <salmankps2001@gmail.com>
2023-06-11 20:28:59 +0530

    Added a new python prog

--- 100 prob/createListLoop.py
+++ 100 prob/createListLoop.py
@@ -3,10 +3,9 @@ data = []
 
 t1 = time.time()
 print("T1: ",t1)
-for element in range(1,101):
+for element in range(1,10000001):
     data.append(f"Element{element}")
 t2 = time.time()
 print("T2: ",t2)
-print("Time taken is: ",t2-t1)
 print(data)
-print(time.clock_gettime())
+print("Time taken is: ",t2-t1)
